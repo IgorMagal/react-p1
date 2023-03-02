271cf936bcc2c791e27f1b7f999730ea50fc21ea
Igor Magalhães <igortrapp30@gmail.com>
2023-03-02 17:23:42 -0400

    Adjustements

--- src/components/PostHeader.tsx
+++ src/components/PostHeader.tsx
@@ -6,14 +6,14 @@ const PostHeader: React.FC<{ handleModal: () => void }> = (props) => {
     <button
       onClick={props.handleModal}
       type="button"
-      className="bg-neutral-700 shadow-2xl hover:bg-neutral-500 text-white font-semibold py-2 my-5 px-4 rounded flex items-center"
+      className="bg-neutral-700 shadow-2xl hover:bg-neutral-500 text-white font-semibold py-2 px-4 rounded flex items-center"
     >
       <MdPostAdd size={30} />
       New post
     </button>
   );
   return (
-    <header className="fixed top-0 left-0 right-0 flex justify-between px-14 items-center bg-neutral-800 border-b-neutral-300 border-b-2 z-10">
+    <header className="fixed py-2 pb-2 top-0 left-0 right-0 flex justify-between px-14 items-center bg-neutral-800 border-b-neutral-300 border-b-2 z-10">
       <div className="flex align-middle justify-center items-center">
         <MdMessage size={40} />
         <p className=" sm:text-xl align-bottom lg:text-3xl font-bold">
