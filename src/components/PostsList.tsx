83a35ec0c0650c0ba123ca5650bc4b0dfbf58443
Igor Magalhães <igortrapp30@gmail.com>
2023-03-04 16:35:54 -0400

    fixes

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -9,7 +9,7 @@ const PostsList: React.FC<{ posts: IPost[] }> = (props) => {
         {posts.reverse().map((post) => (
           <li
             key={post.id}
-            className="w-full xs:w-1/1 sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 2xl:w-1/4 grow"
+            className="w-full max-w-[50%] xs:max-w-[100%] xs:w-1/1 sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 2xl:w-1/4 grow"
           >
             <Post
               author={post.author}
