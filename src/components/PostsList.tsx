b26e39aeafbacca62b49814d3c32b7d3e48c20c4
Igor Magalhães <igortrapp30@gmail.com>
2023-03-04 16:23:58 -0400

    improv post list ui

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -5,11 +5,11 @@ const PostsList: React.FC<{ posts: IPost[] }> = (props) => {
   const { posts } = props;
   return (
     <div className="text-center">
-      <ul className="flex flex-wrap mx-2">
+      <ul className="flex flex-wrap mx-2 my-2">
         {posts.reverse().map((post) => (
           <li
             key={post.id}
-            className="w-full xs:w-1/1 sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 2xl:w-1/4"
+            className="w-full xs:w-1/1 sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 2xl:w-1/4 grow"
           >
             <Post
               author={post.author}
